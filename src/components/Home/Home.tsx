a3a39d04ae1d0bc3420ae595f7bb743704101f77
PatrickTali <ptaliaferro218@gmail.com>
2023-05-02 04:02:49 -0400

    initial

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,95 @@
+import React from 'react';
+import { styled } from '@mui/system';
+import { Button } from '@mui/material';
+import carImage from '../../assets/images/car.jpg';
+import { Link } from 'react-router-dom';
+
+
+interface Props{
+    title: string;
+}
+
+//...interface above
+
+// Create Styled Components with styled-components
+const Root = styled("div")({
+    padding: 0,
+    margin: 0
+})
+const NavbarContainer = styled('div')( {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+})
+const Logo = styled('h1')({
+    margin: '0 0 0 0.45em'
+})
+const LogoA = styled('a')( {
+    color: 'rgb(28,24,22)',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none'
+})
+const LogoNavigation = styled('ul')( {
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    display: 'flex'
+})
+
+const NavA = styled(Link)({
+    display: 'block',
+    padding: '1em',
+    color: 'black'
+})
+const Main = styled('main')( {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${carImage});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
+})
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+
+
+export const Home = ( props:Props) => {
+ 
+    return (
+        <Root>
+            <NavbarContainer>
+                <Logo>
+                    <LogoA href="#">Brand</LogoA>
+                </Logo>
+                <LogoNavigation>
+                    <li>
+                        <NavA to='/'>Home</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/dashboard'>Dashboard</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/signin'>Sign In</NavA>
+                    </li>
+                </LogoNavigation>
+            </NavbarContainer>
+            <Main>
+                <MainText>
+                    <h1>{props.title}</h1>
+                    <p> Cars are cool.</p>
+                    <Button color='primary' variant='contained' component={Link} to ='/dashboard'>Look at Cars</Button>
+                </MainText>
+            </Main>
+
+        </Root>
+    )
+}
